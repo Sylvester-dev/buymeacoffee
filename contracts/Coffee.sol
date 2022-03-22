a24de3d9b996c6574164c17a26288efc0b1bfc4c
Vineet Kumar <vk.vempire@gmail.com>
2022-03-22 16:31:35 +0530

    contract work

--- contracts/Coffee.sol
+++ contracts/Coffee.sol
@@ -5,8 +5,51 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract CoffeePortal {
+    
+    uint256 totalCoffee;
+    address public payable owner;
+
+    event NewCoffee(
+        address indexed from,
+        string message,
+        uint256 timestamp,
+        string name
+    );
 
     constructor() payable {
-        console.log("Yo! I am a Smart Contract");
+        console.log("Yo Yo! Iis a Smart Contract hehe");
+        //user calling this function
+        owner = payable(msg.sender);
     }
+
+// Usinng struct to store. A struct is basically a custom datatype where we can customize what we want to hold inside it.
+    struct Coffee{
+        address sender; //The address of user who want to send me coffee
+        string message; //The message he want to attach
+        string timestamp; //Time transaction took place
+        string name; //Name of the buyer
+    }
+
+//storing array of struct Coffee
+  Coffee[] coffee;
+
+//return struct array coffee
+  function getAllCoffee() public view returns(Coffee[] memory){
+      return coffee;
+  }
+
+//return total coffee bought
+ fuction getTotalCoffee() public view returns(uint256){
+     return totalCoffee;
+ }
+
+ function buyCoffee(string memory _message, string memory _name, uint256 _amount) public payable{
+   uint256 cost = 0.01 ether;
+   require(_amount>=cost,"Not enough fund payed");
+   totalCoffee = totalCoffee + 1;
+   coffee.push(Coffee(msg.sender,_message,block.timestamp,_name));
+   (bool success,)= owner.call{value: _amount}; //sending ether
+   require(success,"Transaction Failed");
+   emit(msg.sender,_message,block.timestamp,_name);
+ }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,18 +1,24 @@
 const hre = require("hardhat");
 
-async function main() {
-  const coffeeContractFactory = await hre.ethers.getContractFactory('CoffeePortal');
-  const coffeeContract = await coffeeContractFactory.deploy();
+const main = async () => {
 
-  // We'll wait until our contract is officially deployed to our local blockchain! Our constructor runs when we actually deploy.
-  await coffeeContract.deployed(); 
-
-  console.log("Coffee Contract deployed to:", coffeeContract.address);
-}
-
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  // This will actually compile our contract and generate the necessary files we need to work with our contract under the artifacts directory.
+    const coffeeContractFactory = await hre.ethers.getContractFactory('CoffeePortal');
+    const coffeeContract = await coffeeContractFactory.deploy();
+  
+    await coffeeContract.deployed(); // We'll wait until our contract is officially deployed to our local blockchain! Our constructor runs when we actually deploy.
+  
+   console.log("Coffee Contract deployed to:", coffeeContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
